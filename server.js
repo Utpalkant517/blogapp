97ec2b4df3621d88a08bbe45f30f2f145d9e55bb
Utpalkant Kumar <utpal1429@gmail.com>
2022-08-06 14:45:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+//components
+import Connection from './database/db.js';
+import Router from './routes/route.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', Router);
+
+if(process.env.NODE_ENV === 'production')
+{
+    app.use(express.static("client/bulid"));
+}
+
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+const URL = process.env.MONGODB_URI ||  `mongodb://${username}:${password}@blog-app-shard-00-00.mv7uc.mongodb.net:27017,blog-app-shard-00-01.mv7uc.mongodb.net:27017,blog-app-shard-00-02.mv7uc.mongodb.net:27017/?ssl=true&replicaSet=atlas-ugvmyr-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+Connection(URL);
+
